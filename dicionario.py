a500e6992e08b6ee2c8f14e327c78a55cf636732
Luiz Fernando <luiz-fernando@outlook.com>
2023-01-24 20:58:38 -0300

    Retorna os n primeiros elementos de um dicionario

---
+++ dicionario.py
@@ -0,0 +1,36 @@
+numeros = {
+    'um': 1,
+    'dois': 2,
+    'tres': 3,
+    'quatro': 4,
+    'cinco': 5,
+    'seis': 6,
+    'sete': 7,
+    'oito': 8,
+    'nove': 9,
+    'dez': 10
+}
+
+
+# print(type(numeros.items()))
+# print(numeros.items())
+
+# print(dict(list(numeros.items())[:3]))
+
+# print(type(numeros.items()))
+print(list(numeros.items()))
+
+numeros = (
+    ('um', 1),
+    ('dois', 2),
+    ('tres', 3),
+)
+
+
+
+
+# print(type(numeros))
+
+# print(dict(numeros))
+
+# print(dict(list(numeros.items())[:3]))
